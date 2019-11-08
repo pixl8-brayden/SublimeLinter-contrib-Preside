83063f3a1db11b200587e83089064500b7c7443f
Brayden Tan <brayden.tan@pixl8.co.uk>
2019-11-08 15:07:24 +0800

    fix indent

--- linter.py
+++ linter.py
@@ -1,9 +1,9 @@
 from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
 
 class Preside(Linter):
-	cmd = '__cmd__'
-	regex = r''
-	multiline = False
-	defaults = {
-		'selector': 'embedding.cfml'
-	}
+    cmd = '__cmd__'
+    regex = r''
+    multiline = False
+    defaults = {
+        'selector': 'embedding.cfml'
+    }
